b542713cbf1d061f4432354dc3d18ffa38f718f5
Andira Suleimenova <suleimenova@kolesa.kz>
2021-06-17 17:25:26 +0600

    Смена директории

---
+++ Урок 10. Тестирование БД/tests/functional/CreateUsersCest.php
@@ -0,0 +1,20 @@
+<?php
+
+class CreateUsersCest
+{
+    /**
+     * before
+     *
+     * @param FunctionalTester $I
+     * return void
+     */
+    public function _before(FunctionalTester $I)
+    {
+        $faker  = $I->get
+    }
+
+    // tests
+    public function tryToTest(FunctionalTester $I)
+    {
+    }
+}
